c3269fead8847df2df0e7538aa9a3136de67bff3
RachelRomee <rachelromee@icloud.com>
2016-06-16 12:09:44 +0200

    Change error on post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,4 +20,6 @@ class PostsController < ApplicationController
   def user
     @user = User.find(params[:user_id])
     @posts = Post.where(user: @user).order(created_at: :desc)
+    @likes = @user.likes.joins(:post).order("posts.created_at DESC")
+  end
 end
